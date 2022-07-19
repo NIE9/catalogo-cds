d3ca8933dd4a0dab17b86ceef2d349c395b7eef6
Thiago Nienov <thiago.nienov@campus.labs>
2022-07-19 17:43:31 -0300

    primeiro commit

---
+++ agenda-master/visao/cadEstilo.php
@@ -0,0 +1,23 @@
+<?php
+if(isset($_POST['botao']) && $_POST['botao']=="Adicionar"){
+	include $_SERVER['DOCUMENT_ROOT']."/agenda/controle/ControleContato.class.php";
+	$cControle = new ControleContato();
+	$cControle->inserir($_POST);
+}
+?>
+<html lang='pt-br'>
+<head>
+<meta charset='utf-8'>
+<title>Catálogo de CDs</title>
+</head>
+<body>
+<form method='post' action='cadEstilo.php'>
+	Estilo: <input type='text' Estilo='nome'>
+	<br>
+	Numero: <input type='tel' name='numero'>
+	<br>
+	<input type='submit' name='botao' value='Adicionar'>
+</form>
+<a href='../index.html'>Voltar</a>
+</body>
+</html>
